6039336216e03b857ea5a099162b0e7109f76eb4
John Hernández <johnenderhernandez@gmail.com>
2021-02-10 19:10:56 -0600

    delete from array

--- js-basic/dataStructs/classes.js
+++ js-basic/dataStructs/classes.js
@@ -24,6 +24,27 @@ class MyArray {
     this.length++;
     return this.data;
   }
+
+  pop(){
+    this.length--;
+    const lasItem = this.data[this.length];
+    delete this.data[this.length];
+    return lasItem;
+  }
+
+  delete(index){
+    const item = this.data[index];
+    shifIndex(index);
+    return item;
+  }
+
+  shifIndex(index){
+    for(let i = index; i < this.length - 1; i++){
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 const myArray = new MyArray();
@@ -34,6 +55,8 @@ console.log(myArray);
 console.log(myArray.get(1));
 console.log(myArray.length);
 
+myArray.pop();
+console.log(myArray);
 
 
 
