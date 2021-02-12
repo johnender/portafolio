fa233041eabe6bc8ee27ccd0b19d467f4b2963c4
John Hernández <johnenderhernandez@gmail.com>
2021-02-12 11:21:00 -0600

    Adding comments to the js R&M files

--- otherProjects/rickAndMorthyList/js/jsAssets/fetchData.js
+++ otherProjects/rickAndMorthyList/js/jsAssets/fetchData.js
@@ -1,17 +1,36 @@
 const requestComplete = 4;
+/*
+  posible states:
+  0: starting
+  1: loading
+  2: loaded
+  3: there is info already
+  4: successful request
+  */
+
 const statusRequest = 200;
+/*
+  State 1xx (100 - 199): request is been processing.
+  State 2xx (200 - 299): the request was received, accepted and correctly processed.
+  State 3xx (300 - 399): further actions needed, usually redirect.
+  State 4xx (400 - 499): client side errors, data request was made wrong.
+  State 5xx (500 - 599): server side errors, execution failed.
+  */
 
 const fetchData = (api_url) => {
   return new Promise((resolve, reject) => {
+    //reference to the object XMLHttpRequest
     const xhttp = new XMLHttpRequest();
+
+    //use the open function of the object XMLHttpRequest parametro1 = el metodo, parametro2 = la url, parametro3 = verificación si es asincrono o no, valor por defecto true
     xhttp.open('GET', api_url, true);
     xhttp.onreadystatechange = () => {
-      if (xhttp.readyState === 4) {
+      if (xhttp.readyState === 4) {//on successful request
         (xhttp.status === 200)
-          ? resolve(JSON.parse(xhttp.responseText))
-          : reject(new Error('Test Error', api_url))
+          ? resolve(JSON.parse(xhttp.responseText))//on successful connection
+          : reject(new Error('Call Error', api_url))//on unsuccessful connection
       }
     }
-    xhttp.send();
+    xhttp.send();//sending the request
   });
 }
--- otherProjects/rickAndMorthyList/js/jsAssets/index.js
+++ otherProjects/rickAndMorthyList/js/jsAssets/index.js
@@ -1,14 +1,15 @@
 const cardsContainer = document.getElementById('cards-container');
 
 const API = 'https://rickandmortyapi.com/api/character/';
-const maxPageForAPI = 34;
-const apiPage = '?page=:page';
+const maxPageForAPI = 34; //how much pages will be called
+const apiPage = '?page=:page'; //to complete the url
 const characters = [];
 
+//this function is asynchronous, thanks to the async
 const getDataRickAndMorty = async (page) => {
   try {
-      const { results } = await fetchData(`${API}${apiPage.replace(':page', `${page}`)}`)
-      results.map(character => {
+      const { results } = await fetchData(`${API}${apiPage.replace(':page', `${page}`)}`) //calls function fecth with the url replacing :page for the actual page number
+      results.map(character => {  //for each one of the results/characters
         const card = cardGenerator(character);
         cardsContainer.appendChild(card);
       });
