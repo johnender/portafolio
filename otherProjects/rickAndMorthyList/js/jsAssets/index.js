79eba64a0f72d24b4a93e62ff1e6aca86e0432a6
John Hernández <johnenderhernandez@gmail.com>
2021-02-11 23:21:32 -0600

    adding to view all characters

--- otherProjects/rickAndMorthyList/js/jsAssets/index.js
+++ otherProjects/rickAndMorthyList/js/jsAssets/index.js
@@ -1,7 +1,7 @@
-const cardsContent = document.getElementById('cards-container');
+const cardsContainer = document.getElementById('cards-container');
 
 const API = 'https://rickandmortyapi.com/api/character/';
-const maxPageForAPI = 25;
+const maxPageForAPI = 34;
 const apiPage = '?page=:page';
 const characters = [];
 
@@ -10,7 +10,7 @@ const getDataRickAndMorty = async (page) => {
       const { results } = await fetchData(`${API}${apiPage.replace(':page', `${page}`)}`)
       results.map(character => {
         const card = cardGenerator(character);
-        cardsContent.appendChild(card);
+        cardsContainer.appendChild(card);
       });
   } catch (error) {
     console.error(error);
