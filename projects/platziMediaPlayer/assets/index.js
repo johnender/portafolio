e1883fdc01ff07cea77e4ee882861987cb2194ab
John Hernández <johnenderhernandez@gmail.com>
2021-02-14 01:32:14 -0600

    separating js code

---
+++ projects/platziMediaPlayer/assets/index.js
@@ -0,0 +1,33 @@
+const  video = document.querySelector("video")//agarrar el video para manipularlo, puede ser video, ya que es el unico elemento con esa etiqueta
+
+const  button = document.querySelector("button")//agarrar el boton para manipularlo, es el unico, sino, por id
+
+/*//ejemplo manejando una funcion prototipada
+function  MediaPlayer(){}//es un objeto, la forma de manejar clases en JS
+MediaPlayer.prototype.play = function() {
+
+  video.play();//el video esta embebido
+
+}
+const  player = new  MediaPlayer();
+button.onclick = () => player.play();//reproducir el video*/
+
+
+//ejemplo con funcion mas extensible
+function  MediaPlayer(config) {//coloca el video que resive como el media actual
+  this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function() {//play o pause a lo que esta en media actualmente
+  if(this.media.paused){
+    this.media.play();
+  } else {
+    this.media.pause()
+  }
+};
+
+const  player = new  MediaPlayer({ el:  video });//se le envia el video actual
+button.onclick = () =>  player.play();
+
+
+video.onclick = () =>  player.play();//para que funcione tambien al tocar el video
