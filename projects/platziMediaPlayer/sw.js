8f1d838c50769899a7d8417edebbc4cbfa6f76ab
John Hernández <johnenderhernandez@gmail.com>
2021-02-18 04:12:34 -0600

    adding cache updating to platziMediaPlayer

--- projects/platziMediaPlayer/sw.js
+++ projects/platziMediaPlayer/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', event =>{
 //evento que responde cuando hay fetch
 self.addEventListener('fetch', event =>{
   const request = event.request;
-  
+
   //solo reaccionar al get
   if(request.method !== "GET"){
     return;
@@ -23,6 +23,10 @@ self.addEventListener('fetch', event =>{
 
   //buscar en cache
   event.respondWith(cachedResponse(request));
+
+
+  //actualizar el cache, por si hay cambios
+  event.waitUntil(updateCache(request));
 })
 
 async function precache() {
@@ -40,9 +44,16 @@ async function precache() {
   ]);
 }
 
-
+//funcion que busca si lo solitado esta en cache o no y lo regresa
 async function cachedResponse(request) {
   const cache = await caches.open(VERSION) //iniciamos abriendo el cache
   const response = await cache.match(request) //preguntamos al cache si ya tiene una copia
   return response || fetch(request) //si el response es undefined, respondemos con lo que nos da la red
+}
+
+//funcion que si puede, actualiza en la cache lo solicitado 
+async function updateCache(request) {
+  const cache = await caches.open(VERSION) //iniciamos abriendo el cache
+  const response = await fetch(request) //preguntamos al cache si ya tiene una copia
+  return cache.put(request, response) //agregamos lo solicitado a la cache
 }
