8224805010ca24de4e2b5433692638a1f35d7be8
johnender <johnenderhernandez@gmail.com>
2025-04-01 08:16:30 -0600

    adding swiper

--- index.js
+++ index.js
@@ -2,7 +2,51 @@
 /* code for the pagination (for the card at the center) */
 
 document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".projects-container--card");
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 5,
+        centeredSlides: true,
+      
+        autoplay: {
+            delay: 2000,
+        },
+        //card size = 395 px
+        breakpoints: {
+            // when window width is >= 830px
+            800: {
+                slidesPerView: 2,
+            },
+            // when window width is >= 1225px
+            1140: {
+                slidesPerView: 3,
+            },
+            // when window width is >= 1620px
+            1900: {
+                slidesPerView: 5,
+            },
+        },
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        /* scrollbar: {
+          el: '.swiper-scrollbar',
+        }, */
+      });
+
+  /* const cards = document.querySelectorAll(".projects-container--card");
   const dots = document.querySelectorAll(".pagination a");
 
   const observerOptions = {
@@ -36,6 +80,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }, observerOptions);
 
   // Observe all cards
-  cards.forEach(card => observer.observe(card));
+  cards.forEach(card => observer.observe(card)); */
 });
 
